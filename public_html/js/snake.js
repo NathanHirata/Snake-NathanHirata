2ef7805128e8eeec43a3c304bb421b83e745d5f1
Nathan Hirata <nathanhirata@yahoo.com>
2014-10-07 15:01:22 -0700

    mile sstones

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,17 +1,24 @@
 var snake;
 
 var context;
-var screenwidth;
-var screenheight;
+var screenWidth;
+var screenHeight;
 
-function gameinitialize() {
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.Width = screenWidth;
+    canvas.Height = screenHeight;
+}
+
+function gameLoop () {
     
 }
 
-function gameloop () {
-    
-}
-
-function gamedraw() {
-    
+function gameDraw() {
+    context.fillStyle = "rgb(129, 193, 92)";
 }
